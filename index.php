550d899433d9a393873144c8aef49a1aaf7bb2de
AnaPaulaFertigBastos <anapfb25@gmail.com>
2024-12-23 20:18:02 -0300

    init

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+ 
+    echo 'Hello World';
+
+  function foo() {
+    echo 'foo';
+  }
+
+  class Teste {
+      public string $nome = 'Ana';
+      public string $email = 'ana@gmail.com';
+     
+
+      public function __construct() {
+        echo __METHOD__;
+      }
+
+      public function bar()
+      {
+        echo __METHOD__;
+      }
+  }
+  echo "<br>";
+  foo();
+
+  echo "<br>";
+  (new Teste)->bar();
+  echo "<br>";
+
+  $teste = new Teste;
+
+  var_dump($teste);
+
+  $teste->bar();
+?>
